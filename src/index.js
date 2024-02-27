e46343b12232626eb3df33460860c596a386a9f0
Moataz024 <prexzcod@gmail.com>
2024-02-27 19:46:13 +0100

    chouaib

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import {ThemeProvider} from '@contexts/themeContext';
+import {ShopProvider} from '@contexts/shopContext';
+import {Provider} from 'react-redux';
+import store from './app/store';
+import './index.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        {/*This is a test*/}
+        <BrowserRouter>
+            <ThemeProvider>
+                <ShopProvider>
+                    <App/>
+                </ShopProvider>
+            </ThemeProvider>
+        </BrowserRouter>
+    </Provider>
+);
